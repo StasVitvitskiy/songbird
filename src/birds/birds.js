a67f0b17237fbca538ecd14b7dab2e4608a2ca4c
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-09-03 13:46:42 -0700

    refactor: move select bird functionality to Redux

--- src/birds/birds.js
+++ src/birds/birds.js
@@ -7,20 +7,59 @@ import win from '../media/win.mp3';
 import lose from '../media/lose.mp3';
 import perfect from '../media/ultimateWin.mp3'
 import {connect} from "react-redux";
-import {setBirdsData} from "~/store";
+import {setBirdsData, setSelectedBird} from "~/store";
 
-let score = 0;
-export let finalScore = 0;
-let result = false;
+function getCorrectCircleClassName(
+    entry,
+    correctAnswer,
+    incorrectAnswers,
+    isAudioPlayed,
+    isAnswerCorrect,
+) {
+  if(isAudioPlayed) {
+    if (incorrectAnswers.includes(entry.id)) {
+      return 'circle-red'
+    }
+    if (entry.id === correctAnswer.id && isAnswerCorrect) {
+      return 'circle-green'
+    }
+  }
+  return 'circle-gray'
+}
+
 class BirdsComponent extends PureComponent {
   componentDidMount() {
     this.props.setBirdsData(birdsData)
-    birdsFunc();
+  }
+
+  onBirdOptionClick = (entry) => {
+    //1 -> immediately
+    return () => {
+      const {isAudioPlayed,setSelectedBird,isAnswerCorrect} = this.props
+      //2 -> when this function is called
+      if(isAudioPlayed && !isAnswerCorrect) {
+        setSelectedBird(entry);
+      }
+    }
   }
 
   render() {
-    const {isAudioPlayed, isAudioPlaying, birdsData, audioIndex} = this.props
+    const {
+      isAudioPlayed,
+      isAudioPlaying,
+      birdsData,
+      audioIndex,
+      selectedBird,
+      incorrectAnswers,
+      audioSrc,
+      isAnswerCorrect,
+    } = this.props
     const birdsGroup = birdsData[audioIndex] || []
+    // correctAnswer type == object (object containing bird data)
+    // should be computed from the store data
+    //find object(el) in birdsGroup array which audio field value === audioSrc
+    const correctAnswer = birdsGroup.find(el => el.audio === audioSrc);
+    correctAnswer && console.log('Correct Answer: ',correctAnswer);
 
     return <div className='bird-container'>
       <div className='birds rounded'>
@@ -30,8 +69,15 @@ class BirdsComponent extends PureComponent {
                 <li
                   key={entry.audio}
                   className="list-group-item li"
+                  onClick={this.onBirdOptionClick(entry)}
                 >
-                  <span className='circle-gray'/>
+                  <span className={getCorrectCircleClassName(
+                      entry,
+                      correctAnswer,
+                      incorrectAnswers,
+                      isAudioPlayed,
+                      isAnswerCorrect,
+                  )}/>
                   {entry.name}
                 </li>)
             )}
@@ -44,172 +90,36 @@ class BirdsComponent extends PureComponent {
           <span>Выберите птицу из списка</span>
         </p>
         <div style={{display: isAudioPlayed ? 'flex' : 'none'}} className="cards">
-          <div className='top-block'>
-            <img className='bird-img' src={bird} alt=""/>
-            <ul className="group">
-              <li className="descr"><h4 className='title'>Ворон</h4></li>
-              <li className="descr"><span className='latin-bird-info'>Corvus corax</span></li>
-              <li className="player">
-                <Player />
-              </li>
-            </ul>
-          </div>
+          {selectedBird && (
+              <div className='top-block'>
+                <img className='bird-img' src={selectedBird.image} alt=""/>
+                <ul className="group">
+                  <li className="descr"><h4 className='title'>{selectedBird.name}</h4></li>
+                  <li className="descr"><span className='latin-bird-info'>{selectedBird.species}</span></li>
+                  <li className="player">
+                    <Player src={selectedBird.audio} />
+                  </li>
+                </ul>
+              </div>
+          )}
           <div className='bird-info-cont'>
             <span className="bird-information">
-              {birds.crow}
+              {selectedBird && selectedBird.description}
               </span>
           </div>
         </div>
       </div>
       <div className='btn-next-level'>
-        <button className='btn next-level'>Next level</button>
+        <button className={isAnswerCorrect ? 'btn next-level btn-next' : 'btn next-level'}>Next level</button>
       </div>
     </div>
   }
 }
 export const Birds = connect(state => state,{
-  setBirdsData
+  setBirdsData,
+  setSelectedBird
 })(BirdsComponent)
-const birds = {
-  crow: 'Ворон – крупная птица. Длина тела достигает 70 сантиметров,' +
-      ' размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра.' +
-      ' В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.',
-}
-const birdsFunc = (index = 0,lang = 'ru') => {
-  const birdsGroup = document.querySelector('.birds-group ul');
-  const nextLevelBtn = document.querySelector('.btn-next-level');
-  let count = 0;
-  const nextBtn = document.querySelector('.btn-next-level button')
-  const titleEl = document.querySelector('.descr .title');
-  const topTitleEl = document.querySelector('.list-group-item h3')
-  const latinBirdInfo = document.querySelector('.latin-bird-info');
-  const birdInfoBlock = document.querySelector('.bird-information');
-  const birdImg = document.querySelectorAll('.bird-img');
-  const audioPlayer = document.querySelectorAll('audio')[1];
-  const cards = document.querySelector('.cards');
-  const p = document.querySelector('p');
-  let to = '';
-  let clicked = false;
-  birdsGroup.addEventListener('click', (e) => {
-    if(!clicked) {
-      document.querySelector('.listen').classList.add('play');
-    } else {
-      const target = e.target;
-      titleEl.innerText = target.innerText;
-      topTitleEl.innerText = target.innerText;
-      cards.style.display = 'flex';
-      p.style.display = 'none';
-      p.classList.remove('initial');
-      latinBirdInfo.innerText = birdsData[index].find((el) => el.name === target.innerText).species;
-      birdInfoBlock.innerText = birdsData[index].find((el) => el.name === target.innerText).description;
-      birdImg[1].src = birdsData[index].find((el) => el.name === target.innerText).image
-      audioPlayer.src = birdsData[index].find((el) => el.name === target.innerText).audio;
-      const score = document.querySelector('.score');
-      let selectedAudio = audioPlayer.src;
-      if(selectedAudio === playedAudio && !nextBtn.classList.contains('btn-next')) {
-        target.childNodes[0].classList.remove('circle-gray');
-        target.childNodes[0].classList.add('circle-green');
-        document.querySelector("#root > div > div.bird-block.rounded.jumbotron > div.margin-left-40px.bird-info.bird-block > ul > li:nth-child(2) > div > audio").pause();
-        let audio = new Audio(win)
-        audio.play();
-        nextBtn.classList.add('btn-next');
-        if(index === 5) {
-          nextBtn.innerText = 'Results';
-        }
-        score.innerText = scoreFunction(count);
-        birdImg[0].src = birdsData[index].find((el) => el.name === target.innerText).image
-      } else {
-        target.childNodes[0].classList.remove('circle-gray');
-        target.childNodes[0].classList.add('circle-red');
-        let audio = new Audio(lose);
-        audio.play()
-        count+=1;
-      }
-    }
-  })
-  nextLevelBtn.addEventListener('click', (e) => {
-    if(!nextBtn.classList.contains('btn-next')) {
-      e.preventDefault();
-    } else if(nextBtn.innerText === 'Results') {
-      finalPage();
-      result = true;
-      const score = document.querySelector('.score');
-      finalScore = score.innerText;
-    } else {
-      clicked = false;
-      switch(to) {
-        case 'Воробьиные':
-          index = 1;
-          break;
-        case 'Лесные птицы':
-          index = 2;
-          break;
-        case 'Певчие птицы':
-          index = 3;
-          break;
-        case 'Хищные птицы':
-          index = 4;
-          break;
-        case 'Морские птицы':
-          index = 5;
-          break;
-        default:
-          index = 0;
-          break;
-      }
-      //audioIndex+=1;
-      topTitleEl.innerText = '******';
-      birdImg[0].src = bird;
-      const active = document.querySelector('.page-item.active');
-      const Menu = document.querySelectorAll('.pagination .page-item');
-      const cards = document.querySelector('.cards');
-      const p = document.querySelector('p');
-      cards.style.display = 'none';
-      p.style.display = 'flex';
-      p.classList.add('initial');
-      const birdsElements = document.querySelectorAll('.birds-group ul li');
-      index+=1;
-      // for(let i = 0; i < birdsGroup.childNodes.length; i++) {
-      //   birdsElements[i].innerHTML =`<span class="circle-gray"></span>${birdsData[index][i].name}`;
-      // }
-      active.classList.remove('active');
-      const progressBar = document.querySelector('.rhap_progress-indicator');
-      progressBar.style.left = '0';
-      const progressFilled = document.querySelector('.rhap_progress-filled');
-      progressFilled.style.width = '0';
-      const curTime = document.getElementById('rhap_current-time');
-      curTime.innerText = '00:00';
-      Menu[index].classList.add('active');
-      count = 0;
-      let activeElement = document.querySelector('.page-item.active');
-      to = activeElement.innerText;
-      switch(to) {
-        case 'Воробьиные':
-          audioPlayer.src = playFunc()
-          break;
-        case 'Лесные птицы':
-          audioPlayer.src = playFunc()
-          break;
-        case 'Певчие птицы':
-          audioPlayer.src = playFunc()
-          break;
-        case 'Хищные птицы':
-          audioPlayer.src = playFunc()
-          break;
-        case 'Морские птицы':
-          audioPlayer.src = playFunc()
-          break;
-        default:
-          index = 0;
-          break;
-      }
-      nextBtn.classList.remove('btn-next');
-      if (index === 6) {
-        index = 0;
-      }
-    }
-  })
-}
+
 export const getRandomSong = (index = 0) => {
   return birdsData[index].map((el) => {
     return el.audio
@@ -218,29 +128,7 @@ export const getRandomSong = (index = 0) => {
 const getRandomArbitrary = (min, max) =>  {
   return Math.floor(Math.random() * (max - min) + min);
 }
-const scoreFunction = (attempts) => {
-  switch(attempts) {
-    case 0:
-      score+=5;
-      break;
-    case 1:
-      score+=4;
-      break;
-    case 2:
-      score+=3;
-      break;
-    case 3:
-      score+=2;
-      break;
-    case 4:
-      score+=1;
-      break;
-    default:
-      score+=0;
-      break;
-  }
-  return score;
-}
+
 const finalPage = () => {
   const birdBlock = document.querySelector('.bird-block');
   const birdContainer = document.querySelector('.bird-container');
