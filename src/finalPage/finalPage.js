4a1a2515e9fdc6d94e9000952c8633779dd93701
Stanislav Vitvitskiy <stanislavvitvitskiy@Stanislavs-MacBook-Pro.local>
2020-09-09 14:50:03 -0700

    refactor: finish finalPage

--- src/finalPage/finalPage.js
+++ src/finalPage/finalPage.js
@@ -1,13 +1,34 @@
 import React, {PureComponent} from 'react'
 import './finalPage.css'
+import {connect} from "react-redux";
+import {tryAgain} from "~/store";
+import {withRouter} from "react-router";
 
-export class FinalPage extends PureComponent {
-  render() {
-    return <div className='final-page jumbotron'>
-      <h1 className='perfect'>ИДЕАЛЬНЫЙ SCORE!!!</h1>
-      <h1 className='congrats'>Поздравляем!</h1>
-      <h6>Вы прошли викторину и набрали <span className='score-span'>0</span> из 30 возможных баллов</h6>
-      <button className='btn btn-next try-again'>Попробовать еще раз</button>
-    </div>
-  }
-}
+export const FinalPage = connect(state => state, {
+    tryAgain,
+})(
+    withRouter(
+        class FinalPage extends PureComponent {
+          onTryAgainClick = () => {
+            const {tryAgain, history} = this.props;
+            tryAgain();
+            history.push('/birds/0');
+          }
+          render() {
+            const {score,birdsData = [],audioIndex} = this.props;
+            const perfectScore = ((birdsData[audioIndex] || []).length - 1) * birdsData.length;
+            const isPerfectScore = score === perfectScore;
+            return <div className='final-page jumbotron'>
+              {isPerfectScore && (<h1 className='perfect'>ИДЕАЛЬНЫЙ SCORE!!!</h1>)}
+              <h1 className='congrats'>Поздравляем!</h1>
+              <h6>Вы прошли викторину и набрали <span className='score-span'>{score}</span> из {perfectScore} возможных баллов</h6>
+              <button
+                  onClick={this.onTryAgainClick}
+                  className='btn btn-next try-again'>
+                Попробовать еще раз
+              </button>
+            </div>
+          }
+        }
+    )
+)
